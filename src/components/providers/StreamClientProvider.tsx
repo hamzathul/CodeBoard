bc12f56ea76e6b9866d46227a9cf293b924e3a3a
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2025-02-15 13:31:45 +0530

    stream provider in client side

---
+++ src/components/providers/StreamClientProvider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
+import { ReactNode, useEffect, useState } from "react";
+import LoaderUI from "../LoaderUI";
+
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [StreamVideoClient, setStreamVideoClient] =
+    useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+
+    const client = new StreamVideoClient({
+      apiKey: process.env.NEXT_PUBLIC_STREAM_API_KEY,
+      user: {
+        id: user.id,
+        name: user.firstName || "" + " " + user.lastName || "" || user.id,
+        image: user.imageUrl,
+      },
+      tokenProvider: streamTokenProvider,
+    });
+    setStreamVideoClient(client);
+  }, [user, isLoaded]);
+
+  if (!StreamVideoClient) return <LoaderUI />;
+  return <StreamVideo client={StreamVideoClient}>{children}</StreamVideo>;
+};
+
+export default StreamVideoProvider;
